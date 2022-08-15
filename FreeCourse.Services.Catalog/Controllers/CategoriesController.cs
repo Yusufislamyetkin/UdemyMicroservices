765abcd3aa6dd45d3ba9c7984b27ef101c64d550
yusuf <yusuf>
2022-08-15 15:12:21 +0300

    15:12

---
+++ FreeCourse.Services.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,43 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    internal class CategoriesController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return CreateActionResultInstance(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var categories = await _categoryService.GetByIdAsync(id);
+            return CreateActionResultInstance(categories);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            var category = await _categoryService.CreateAsync(categoryDto);
+            return CreateActionResultInstance(category);
+        }
+
+
+    }
+}
---
+++ FreeCourse.Services.Catalog/Controllers/CoursesController.cs
@@ -0,0 +1,71 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    internal class CoursesController : CustomBaseController
+    {
+        private readonly ICourseService _courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _courseService.GetByIdAsync(id);
+
+            // Retoru ok, return notfound vs dönecek yeri teker teker yazmak yerine custombasecontrollerdan return türetiyoruz.
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAllByUserId(string id)
+        {
+            var response = await _courseService.GetAllByUserId(id);
+
+            // Retoru ok, return notfound vs dönecek yeri teker teker yazmak yerine custombasecontrollerdan return türetiyoruz.
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _courseService.GetAllAsync();
+
+            // Retoru ok, return notfound vs dönecek yeri teker teker yazmak yerine custombasecontrollerdan return türetiyoruz.
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CourseCreateDto courseCreateDto)
+        {
+            var response = await _courseService.CreateAsync(courseCreateDto);
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(CourseUpdateDto courseUpdateDto)
+        {
+            var response = await _courseService.UpdateAsync(courseUpdateDto);
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var response = await _courseService.DeleteAsync(id);
+
+            // Retoru ok, return notfound vs dönecek yeri teker teker yazmak yerine custombasecontrollerdan return türetiyoruz.
+            return CreateActionResultInstance(response);
+        }
+    }
+}
