ca8391517e2e572d7a30f99fecbf02bc485cf1b3
yusuf <yusuf>
2022-08-17 14:08:41 +0300

    14:08

---
+++ FreeCourse.Services.Discount/Service/IDiscountService.cs
@@ -0,0 +1,16 @@
+﻿using FreeCourse.Shared.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Discount.Service
+{
+    public interface IDiscountService
+    {
+        Task<Response<List<Models.Discount>>>GetAll();
+        Task<Response<Models.Discount>>GetById(int id);
+
+        Task<Response<NoContent>> Save(Models.Discount discount);
+        Task<Response<NoContent>> Update(Models.Discount discount);
+
+    }
+}
