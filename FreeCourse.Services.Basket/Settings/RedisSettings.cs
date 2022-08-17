c02d37e4ecb2769112aeeb332b741945fc9db5a9
yusuf <yusuf>
2022-08-17 09:51:55 +0300

    9:51

--- FreeCourse.Services.Basket/Settings/RedisSettings.cs
+++ FreeCourse.Services.Basket/Settings/RedisSettings.cs
@@ -2,6 +2,7 @@
 {
     public class RedisSettings
     {
+        // Burayı Catalog da olduğu gibi interface üzerinden implente etmeye çalışacağız.
         public string Host { get; set; }
         public int Port { get; set; }
     }
