42efdfee5bc5c5fe541a34603af6a7454d33f110
yusuf <yusuf>
2022-08-18 11:34:42 +0300

    11:34

---
+++ FreeCourse.Services.Order.Domain/OrderAggregate/Address.cs
@@ -0,0 +1,49 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class Address:ValueObject
+    {
+        // Entityleri ulaşılabilir yapar.
+        public Address(string province, string district, string street, string zipCode, string line)
+        {
+            Province = province;
+            District = district;
+            Street = street;
+            ZipCode = zipCode;
+            Line = line;
+        }
+
+        public string Province { get;private set; }
+        public string District { get;private set; }
+        public string Street { get; private set; }
+        public string ZipCode { get; private set; }
+
+        public string Line { get;private set; }
+
+        // Direkt olarak veriyi döndürür. Ya da bussines ile döndürebilirsin aşşağıdaki setzipcode örneğindeki gibi
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Province;
+            yield return District;
+            yield return Street;
+            yield return ZipCode;
+            yield return Line;
+        }
+
+
+        // Geleneksel mimariden farklı olarak burada bussines kodu yazabiliriz. Örnek olarak belirli şartları sağlayanı return ederek set edebilir. Veritabanına veriyi set ederken
+        // Büyük harfe dönüştürüp öyle kaydedebiliriz.
+        public void SetZipCode(string zipCode)
+        {
+            // Business Code
+
+            ZipCode = zipCode;
+        }
+    }
+}
---
+++ FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,48 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    //EFCORE features
+
+    // - Owned Types
+    // - Shadow Property
+    // - Backing Field
+
+    public class Order : Entity, IAggregateRoot
+    {
+        public DateTime CreatedDate { get; set; }
+
+        //Owned Type denir bu yapıya.
+        public Address Address { get; set; }
+        public string BuyerId { get; set; }
+
+        // Private olarak belirliyoruz ki kimse direkt olarak erişemesin ve direkt olarak orderıtem ekleyemesin.Backing fields olarak isimlendirilir
+        private readonly List<OrderItem> _orderItems;
+        private IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+        public Order(Address address, string buyerId)
+        {
+            _orderItems = new List<OrderItem>();
+            CreatedDate = DateTime.Now;
+            Address = address;
+            BuyerId = buyerId;
+
+        }
+        public void AddOrderItem(string productId, string productName, decimal price, string pictureUrl)
+        {
+            var existProduct = _orderItems.Any(x => x.ProductId == productId);
+            if (!existProduct)
+            {
+                var newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+                _orderItems.Add(newOrderItem);
+            }
+        }
+
+        public decimal GetTotalPrice => _orderItems.Sum(x => x.Price);
+    }
+}
