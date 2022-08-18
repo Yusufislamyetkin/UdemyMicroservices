e0e5dafa2534a9b5edbad346581100bab00f09fd
yusuf <yusuf>
2022-08-18 12:03:45 +0300

    12:03

--- FreeCourse.Services.Order.Application/Mapping/CustomMapping.cs
+++ FreeCourse.Services.Order.Application/Mapping/CustomMapping.cs
@@ -13,9 +13,9 @@ namespace FreeCourse.Services.Order.Application.Mapping
     {
         public CustomMapping()
         {
-            CreateMap<Order.Domain.OrderAggregate.Order, OrderDto>();
-            CreateMap<Order.Domain.OrderAggregate.OrderItem, OrderItemDto>();
-            CreateMap<Order.Domain.OrderAggregate.Address, AddressDto>();
+            CreateMap<Order.Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+            CreateMap<Order.Domain.OrderAggregate.OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Order.Domain.OrderAggregate.Address, AddressDto>().ReverseMap();
         }
     }
 }
---
+++ FreeCourse.Services.Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Application.Mapping
+{
+    public static class ObjectMapper
+    {
+        // Lazy kullandığımız da otomatik olarak build sırasında siseme eklenmez. Ne zamanki biz lazy ile belirlenen yapıya erişiriz o zaman sisteme dahil edilir.
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CustomMapping>();
+            });
+            return config.CreateMapper();
+        });
+
+        public static IMapper Mapper => lazy.Value;
+    }
+}
