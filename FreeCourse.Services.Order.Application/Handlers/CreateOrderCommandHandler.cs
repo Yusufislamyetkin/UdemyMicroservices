2a6d193d36abe97fc929f7229a6745e23993b7ea
yusuf <yusufislamyetkin@hotmail.com>
2022-08-18 15:10:42 +0300

    15:10

--- FreeCourse.Services.Order.Application/Handlers/CreateOrderCommandHandler.cs
+++ FreeCourse.Services.Order.Application/Handlers/CreateOrderCommandHandler.cs
@@ -13,18 +13,39 @@ using System.Threading.Tasks;
 
 namespace FreeCourse.Services.Order.Application.Handlers
 {
+    // CQRS Yapımızın Command kısmında yani write kısmında çalışıyoruz.
     public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Response<CreatedOrderDto>>
     {
-        private  OrderDbContext _context;
-        
+        // Veritabanı bağlantımızı yaptık istersek repository ile de çalışabilirdik. Böylesi daha kolay ve pratik diye tercih ettik.
+        private OrderDbContext _context;
+
         public CreateOrderCommandHandler(OrderDbContext context)
         {
             _context = context;
         }
 
-        public Task<Response<CreatedOrderDto>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        // CancellationToken işlem iptali sırasında hata fırlatır ve işlemin devam etmesini engeller.
+        public async Task<Response<CreatedOrderDto>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
         {
-          var newAddress = new Address(request.OrderItems)
+            // Siparişimizi oluşturmak için ilk olarak gönderilen requestteki adress kısmını dolduruyoruz. Bu kısım order ile direkt bağlı olduğundan
+            // direkt ordercreate kısmında inşa ediyoruz.
+            var newAddress = new Address(request.AddressDto.Province, request.AddressDto.District, request.AddressDto.Street, request.AddressDto.ZipCode, request.AddressDto.Line);
+
+            //Requestten gelen adresi doldurduktan sonra requestten gelen BuyerId ile , yeni adresimizi siparişimize ekliyoruz. 
+            Domain.OrderAggregate.Order newOrder = new Domain.OrderAggregate.Order(request.BuyerId, newAddress);
+
+            // Sıra geldi siparişimizdeki itemleri(ürünleri eklemede) 
+            // requestin içersindeki ürünleri teker teker özniteliklerle siparişimizin içine ekliyoruz.
+            request.OrderItems.ForEach(x =>
+            {
+                newOrder.AddOrderItem(x.ProductId, x.ProductName, x.Price, x.PictureUrl);
+            });
+
+            // en sonunda da içerisine ürünler dolan, satın alanı ve adresi belli olan siparişimizi veritabanına kaydediyoruz.
+            _context.Orders.Add(newOrder);
+            var result = await _context.SaveChangesAsync();
+
+            return Response<CreatedOrderDto>.Success(new CreatedOrderDto { OrderId = newOrder.Id }, 200);
         }
     }
 }
