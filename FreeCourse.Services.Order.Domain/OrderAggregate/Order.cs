f267f6ab41e2db62820d37f2c79737bc89169845
yusuf <yusuf>
2022-08-18 14:03:24 +0300

    14:03

--- FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
+++ FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
@@ -7,38 +7,42 @@ using System.Threading.Tasks;
 
 namespace FreeCourse.Services.Order.Domain.OrderAggregate
 {
-    //EFCORE features
-
-    // - Owned Types
-    // - Shadow Property
-    // - Backing Field
-
+    //EF Core features
+    // -- Owned Types
+    // -- Shadow Property
+    // -- Backing Field
     public class Order : Entity, IAggregateRoot
     {
-        public DateTime CreatedDate { get;private set; }
+        public DateTime CreatedDate { get; private set; }
 
-        //Owned Type denir bu yapıya.
-        public Address Address { get;private set; }
-        public string BuyerId { get;private set; }
+        public Address Address { get; private set; }
 
-        // Private olarak belirliyoruz ki kimse direkt olarak erişemesin ve direkt olarak orderıtem ekleyemesin.Backing fields olarak isimlendirilir
+        public string BuyerId { get; private set; }
+
         private readonly List<OrderItem> _orderItems;
-        private IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
 
-        public Order(Address address, string buyerId)
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+        public Order()
+        {
+        }
+
+        public Order(string buyerId, Address address)
         {
             _orderItems = new List<OrderItem>();
             CreatedDate = DateTime.Now;
-            Address = address;
             BuyerId = buyerId;
-
+            Address = address;
         }
+
         public void AddOrderItem(string productId, string productName, decimal price, string pictureUrl)
         {
             var existProduct = _orderItems.Any(x => x.ProductId == productId);
+
             if (!existProduct)
             {
                 var newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+
                 _orderItems.Add(newOrderItem);
             }
         }
