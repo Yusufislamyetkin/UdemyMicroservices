0f05a62215def566f2ddb12d0fb071c735e12127
yusuf <yusufislamyetkin@hotmail.com>
2022-08-19 13:42:15 +0300

    13:42

--- FreeCourse.Web/Models/ClientSettings.cs
+++ FreeCourse.Web/Models/ClientSettings.cs
@@ -3,5 +3,13 @@
     // Servislerle iletişim kurmamız için göndereceğimiz Client ID, Client Screet
     public class ClientSettings
     {
+        public Client WebClient { get; set; }
+        public Client WebClientForUser { get; set; }
+    }
+
+    public class Client
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
     }
 }
---
+++ FreeCourse.Web/Services/IdentityService.cs
@@ -0,0 +1,43 @@
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourse.Web.Models;
+using FreeCourse.Web.Models.Input;
+using FreeCourse.Web.Services.Interfaces;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly HttpClient _client;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ClientSettings _clientSettings;
+        private readonly ServiceApiSettings _serviceApiSettings;
+
+        public IdentityService(HttpClient client, IHttpContextAccessor httpContextAccessor, IOptions<ClientSettings> clientSettings, IOptions<ServiceApiSettings> serviceApiSettings)
+        {
+            _client = client;
+            _httpContextAccessor = httpContextAccessor;
+            _clientSettings = clientSettings.Value;
+            _serviceApiSettings = serviceApiSettings.Value;
+        }
+
+        public Task<TokenResponse> GetAccessTokenByRefreshToken()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task RevokeRefreshToken()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Response<bool>> SıgnIn(SigninInput signInInput)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
