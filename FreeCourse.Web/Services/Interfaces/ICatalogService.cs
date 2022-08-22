c408385f40d7a8ac9e2b671cfe8f9e4620b1b156
yusuf <yusufislamyetkin@hotmail.com>
2022-08-22 11:35:08 +0300

    11:35

--- FreeCourse.Web/Services/Interfaces/ICatalogService.cs
+++ FreeCourse.Web/Services/Interfaces/ICatalogService.cs
@@ -1,7 +1,20 @@
-﻿namespace FreeCourse.Web.Services.Interfaces
+﻿using FreeCourse.Web.Models.Catalogs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Interfaces
 {
     public interface ICatalogService
     {
+        // İlk önce tüm kursları alalım.
+        Task<List<CourseViewModel>> GetAllCourse();
+        Task<List<CourseViewModel>> GetlAllCourseByUserIdAsync(string userId);
+      
+        Task<CourseViewModel> GetCourseByIdAsync(string courseId);
+        Task<bool> DeleteCourseAsync(string courseId);
+        Task<bool> AddCourseAsync(CourseCreateInput  courseCreateInput);
+
+
 
     }
 }
