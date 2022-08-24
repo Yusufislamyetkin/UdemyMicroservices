aa22fa887b4defca8fa78b836b1c48f881a768c0
yusuf <yusufislamyetkin@hotmail.com>
2022-08-24 12:59:38 +0300

    12:59

---
+++ FreeCourse.Services.Order.Application/Consumer/CreateOrderMessageCommandConsumer.cs
@@ -0,0 +1,26 @@
+﻿using FreeCourse.Services.Order.Infrastructure;
+using FreeCourse.Shared.Messages;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Application.Consumer
+{
+    //internal class CreateOrderMessageCommandConsumer : IConsumer<CreateOrderMessageCommand>
+    //{
+    //    private readonly OrderDbContext _orderDbContext;
+
+    //    public CreateOrderMessageCommandConsumer(OrderDbContext orderDbContext)
+    //    {
+    //        _orderDbContext = orderDbContext;
+    //    }
+
+    //    public Task Consume(ConsumeContext<CreateOrderMessageCommand> context)
+    //    {
+    //      var newAddress = new Domain.OrderAggregate.Address(context.Message.Province)
+    //    }
+    //}
+}
